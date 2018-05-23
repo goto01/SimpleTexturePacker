f7218aa6059a107df4b793bdfb6b8ed2cc2c830d
Goto01 <kirill.tkachev.ktkach@gmail.com>
2018-05-23 14:23:39 +0300

    Fixed sprite preview

--- Simple Texture Packer/Assets/TexturePacker/Scripts/Editor/Properties/SpriteProperty.cs
+++ Simple Texture Packer/Assets/TexturePacker/Scripts/Editor/Properties/SpriteProperty.cs
@@ -10,75 +10,95 @@ namespace TexturePacker.Editor.Properties
 	[CustomPropertyDrawer(typeof(SpritePropertyAttribute))]
 	public class SpriteProperty : PropertyDrawer
 	{
+		private const float EditorOffset = 16f + 2f;
 		private const string Preview = "Sprite Property Preiew Enalbing";
 		private const float SpritePreviewHeight = 100;
 		private const float Separator = .8f;
+		private Sprite _prevSprite;
 		private Sprite _selectedSprite;
-		private SerializedProperty _serializedProperty;
-		private bool _heightMode;
+		private Sprite _sprite;
+		private bool _previewPossible;
+		private Texture2D _previewTexture;
+		private GUIStyle _previewTextureStyle;
 
+		private bool NeedRefreshPreviewTexture{get { return _prevSprite != _sprite; }}
+		private bool PreviewNotPossible { get { return _previewPossible = (_sprite == null); } }
 		private bool PreviewFoldout
 		{
 			get { return PlayerPrefs.GetInt(Preview, -1) > 0; }
 			set { PlayerPrefs.SetInt(Preview, value ? 1 : -1); }
 		}
-		private Sprite Sprite
-		{
-			get
-			{
-				if (_serializedProperty == null) return null;
-				return (Sprite) _serializedProperty.objectReferenceValue;
-			}
-		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			var height = _heightMode ? base.GetPropertyHeight(property, label) : EditorGUIUtility.singleLineHeight * 2 + EditorGUIUtility.standardVerticalSpacing * 3;
-			if (PreviewFoldout) height += SpritePreviewHeight + 2 * EditorGUIUtility.standardVerticalSpacing;
+			var height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+			if (!PreviewNotPossible) height += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+			if (!PreviewNotPossible && PreviewFoldout) height += EditorGUIUtility.standardVerticalSpacing + SpritePreviewHeight;
+			height += EditorGUIUtility.standardVerticalSpacing;
 			return height;
 		}
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			_serializedProperty = property;
-			if (_selectedSprite != null)
-			{
-				property.objectReferenceValue = _selectedSprite;
-				_selectedSprite = null;
-			}
+			Init();
+			_sprite = property.objectReferenceValue as Sprite;
+			ApplySelectedSprite(property);
 			position.height = EditorGUIUtility.singleLineHeight;
-			var tempWidth = position.width;
-			var tempX = position.x;
-			var rect = position;
+			DrawSpriteSelector(position, property, label);
+			position.y += EditorOffset;
+			if (PreviewNotPossible) return;
+			DrawFoldout(position);
+			if (!PreviewFoldout) return;
+			position.y += EditorOffset;
+			DrawSpritePreview(position);
+		}
+
+		private void Init()
+		{
+			if (_previewTextureStyle == null) _previewTextureStyle = new GUIStyle(); 
+		}
+
+		private void ApplySelectedSprite(SerializedProperty property)
+		{
+			if (_selectedSprite == null) return;
+			property.objectReferenceValue = _selectedSprite;
+			_selectedSprite = null;
+		}
+		
+		private void DrawSpriteSelector(Rect rect, SerializedProperty property, GUIContent label)
+		{
+			var width = rect.width;
 			rect.width *= Separator;
 			EditorGUI.PropertyField(rect, property, label);
 			rect.x += rect.width;
-			rect.width = position.width * (1 - Separator);
+			rect.width = width * (1 - Separator);
 			if (GUI.Button(rect, "Select")) Dialog.ShowDialog<SpriteSelectorDialogWindow>("Sprite selector dialog", DialogType.YesNo).Yes += OnYes;
-			rect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-			rect.width = tempWidth;
-			rect.x = tempX;
-			if ((_heightMode = (Sprite == null))) return;
-			PreviewFoldout = EditorGUI.Foldout(rect, PreviewFoldout, Sprite.name);
-			if (!PreviewFoldout) return;
+		}
+
+		private void DrawFoldout(Rect rect)
+		{
+			PreviewFoldout = EditorGUI.Foldout(rect, PreviewFoldout, _sprite.name);
+		}
+
+		private void DrawSpritePreview(Rect rect)
+		{
+			if (NeedRefreshPreviewTexture) RefreshPreviewTexture();
 			rect.height = SpritePreviewHeight;
-			var spriteRect = new Rect(Sprite.rect.x/Sprite.texture.width,
-				Sprite.rect.y/Sprite.texture.height,
-				Sprite.rect.width/Sprite.texture.width,
-				Sprite.rect.height/Sprite.texture.height);
-			rect.width = rect.height * Sprite.rect.width / Sprite.rect.height;
-			rect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-			var texture = new Texture2D((int)Sprite.rect.width, (int)Sprite.rect.height, TextureFormat.ARGB32, false);
-			texture.SetPixels(Sprite.texture.GetPixels((int)Sprite.textureRect.x, (int)Sprite.textureRect.y,
-				(int)Sprite.textureRect.width, (int)Sprite.textureRect.height));
+			rect.width = rect.height * _previewTexture.width / _previewTexture.height;
+			GUI.Box(rect, GUIContent.none);
+			GUI.Label(rect, GUIContent.none, _previewTextureStyle);
+		}
+
+		private void RefreshPreviewTexture()
+		{
+			var texture = new Texture2D((int)_sprite.rect.width, (int)_sprite.rect.height, TextureFormat.ARGB32, false);
+			texture.SetPixels(_sprite.texture.GetPixels((int)_sprite.textureRect.x, (int)_sprite.textureRect.y,
+				(int)_sprite.textureRect.width, (int)_sprite.textureRect.height));
 			texture.filterMode = FilterMode.Point;
 			texture.Apply();
-			rect.height = SpritePreviewHeight;
-			rect.width = rect.height * texture.width / texture.height;
-			var style = new GUIStyle();
-			style.normal.background = texture;
-			GUI.Box(rect, GUIContent.none);
-			GUI.Label(rect, GUIContent.none, style);
+			_prevSprite = _sprite;
+			_previewTexture = texture;
+			_previewTextureStyle.normal.background = _previewTexture;
 		}
 
 		private void OnYes(SpriteSelectorDialogWindow sender)
